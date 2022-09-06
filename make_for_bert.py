d5b6661054d3ba5f5c8ff03e3df67fcbc206f5a7
Bachstelze <Bachstelze@users.noreply.github.com>
2022-09-06 11:00:21 +0200

    Update make_for_bert.py
    add all languages

--- make_for_bert.py
+++ make_for_bert.py
@@ -1,22 +1,26 @@
 import os
 
-with open('forbert.tsv', 'w') as outfile:
-    for filename in os.listdir('.'):
-        if filename.endswith('.txt'):
-            case = filename.split('.txt')[0]
-            with open(filename, 'r') as infile:
-                g = ""
-                ug = ""
-                for line in infile:
-                    line_tuple = line.strip().split('\t')
-                    if line_tuple[0] == "True":
-                        is_grammatical = True
-                    else:
-                        is_grammatical = False
-                    sentence = line_tuple[1]
+languages = ["de","en","ru","fr","he"]
 
-                    if is_grammatical:
-                        g = sentence
-                    else:
-                        ug = sentence
-                        outfile.write(case+'\t'+'de'+'\t'+g+'\t'+ug+'\n')
+for lang in languages:
+  directory = './clams/'+lang+'_evalset/'
+  with open(lang+'_forbert.tsv', 'w') as outfile:
+      for filename in os.listdir(directory):
+          if filename.endswith('.txt'):
+              case = filename.split('.txt')[0]
+              with open(directory+filename, 'r') as infile:
+                  g = ""
+                  ug = ""
+                  for line in infile:
+                      line_tuple = line.strip().split('\t')
+                      if line_tuple[0] == "True":
+                          is_grammatical = True
+                      else:
+                          is_grammatical = False
+                      sentence = line_tuple[1]
+
+                      if is_grammatical:
+                          g = sentence
+                      else:
+                          ug = sentence
+                          outfile.write(case+'\t'+lang+'\t'+g+'\t'+ug+'\n')
