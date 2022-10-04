887bfc174db82cfd3243bd238c13b94d6d5aa3b5
Bachstelze <Bachstelze@users.noreply.github.com>
2022-10-04 11:23:36 +0200

    Update multilingual_reproduction.py
    add xlm roberta compability

--- bert-syntax/multilingual_reproduction.py
+++ bert-syntax/multilingual_reproduction.py
@@ -6,18 +6,24 @@ import csv
 from collections import Counter
 
 # Use english model
-model_name = 'bert-base-multilingual-cased' # multilingual
+model_name = 'xlm-roberta-base' # multilingual roberta
+#model_name = 'bert-base-multilingual-cased' # multilingual
 #model_name = 'bert-base-uncased' # monolingual
-en_tokenizer = AutoTokenizer.from_pretrained(model_name)
-en_model = AutoModelForMaskedLM.from_pretrained(model_name, is_decoder=False)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForMaskedLM.from_pretrained(model_name, is_decoder=False)
 #model.eval()
 
 fill_pipeline = pipeline("fill-mask",
-               model=en_model,
-               tokenizer=en_tokenizer,
+               model=model,
+               tokenizer=tokenizer,
                device=0)
 
 languages = ["de","en","ru","fr","he"]
+
+def get_pipeline_output(input, target_token, batch_size):
+    if model_name == 'xlm-roberta-base':
+        target_token = '▁' + target_token
+    return fill_pipeline(input, targets=[target_token], batch_size=batch_size)
 
 def read_test(lang, directory):
   cc = Counter()
@@ -69,7 +75,7 @@ def prepare_dataset_dictionaries(test_list, target_tokens):
     for line_number, line in enumerate(test_list):
         case,tp,s,word1,word2 = line
         pre,target,post=s.split('***')
-        combined_sentence = pre + en_tokenizer.mask_token + post
+        combined_sentence = pre + tokenizer.mask_token + post
 
         datasets[word1].append(combined_sentence)
         dataset_mapping[word1].append(line_number)
@@ -85,9 +91,10 @@ def prepare_dataset_dictionaries(test_list, target_tokens):
 
 def get_out_of_vocabulary_tokens(target_tokens):
     out_of_vocabulary_tokens = []
+    input = tokenizer.mask_token
 
     for token in target_tokens:
-        token_str = fill_pipeline(en_tokenizer.mask_token, targets=[token])[0]['token_str']
+        token_str = get_pipeline_output(input, token, 1)[0]['token_str']
         if token_str != token:
             out_of_vocabulary_tokens.append(token)
 
@@ -100,8 +107,9 @@ def get_out_of_vocabulary_tokens(target_tokens):
     print("full token list:")
     print(target_tokens)
 
+    # there should be no more prints
     for token in target_tokens:
-        fill_pipeline(en_tokenizer.mask_token, targets=[token])
+        get_pipeline_output(input, token, 1)
 
     return out_of_vocabulary_tokens
 
@@ -109,7 +117,7 @@ def map_reduce_scores(batch_size, target_tokens, datasets, target_scores, datase
     for target_token in target_tokens:
         print(target_token)
         for line in datasets[target_token]:
-            filled_line = fill_pipeline(line, targets=[target_token], batch_size=batch_size)
+            filled_line = get_pipeline_output(line, target_token, batch_size)
             target_scores[target_token].append(filled_line[0]["score"])
 
     # summarize to scores for each line
@@ -140,7 +148,7 @@ def print_results(test_list, target_tokens, line_result, result_file):
             print(correctness, case, lang, w1, str(score1), w2, str(score2), sentence,file=result_file)
 
 for lang in languages:
-    result_file=open(lang+'_result_03_10.txt', 'a')
+    result_file=open(lang+'_result_xlm_04_10.txt', 'a')
     test_list = read_test(lang, ".")
     target_tokens = get_target_tokens(test_list)
     datasets, dataset_mapping, dataset_cases, target_scores, line_result = prepare_dataset_dictionaries(test_list, target_tokens)
